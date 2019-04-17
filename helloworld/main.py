85fda33419291cbf403e87ad590d9f744e1d17a5
ptddev <45938253+ptddev@users.noreply.github.com>
2019-04-17 10:40:20 +0200

    Update main.py
    To differentiate from local src

--- helloworld/main.py
+++ helloworld/main.py
@@ -3,5 +3,5 @@ import sys
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    print "Hello world from python!"
+    print "Hello world from python, fetched from GitHub!"
     return 0
